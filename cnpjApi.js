cbeada1a911f0a90f1c0e0e6b5ca993ae0bfd502
Felipe Rosene Klapsztein <feliperosenek@gmail.com>
2024-02-17 15:21:22 -0300

    Create cnpjApi.js

---
+++ cnpjApi.js
@@ -0,0 +1,190 @@
+const consultarCNPJ = require("consultar-cnpj");
+const token = "DHoTG3PWtsUGaC1xkVvCMzOWbdKAjZu7L7l4r4fPOWzb";
+const Sequelize = require("sequelize");
+const { QueryTypes } = require("sequelize");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const sequelize = new Sequelize("eduard72_" + process.env.DATABASE + "", "eduard72_wp625", "37@S0DSm(p", {
+  host: "sh-pro20.hostgator.com.br",
+  dialect: "mysql",
+  define: {
+    freezeTableName: true,
+    timestamps: false,
+  },
+  logging: false,
+});
+
+sequelize
+  .authenticate()
+  .then(() => {})
+  .catch(err => {
+    console.error("Erro ao conectar a banco de dados: ", err);
+  });
+
+async function getCNPJ() {
+  var page = 0;
+
+  try {
+    // --> Pesquisa os CNAE's no banco
+    var cnaes = await sequelize.query("SELECT id, cnae FROM "+process.env.TABLE+" WHERE statusBot" + process.env.BOT + "=0", {
+      type: QueryTypes.SELECT,
+    });
+
+    console.log(cnaes)
+
+    // --> Abre um loop para pesquisar sobre os cnaes
+    for (i = 0; i < cnaes.length; i++) {
+      cnaesSearch = cnaes[i].cnae;
+
+      // pesquisa por cnae
+      const data = await consultarCNPJ.pesquisa(
+        {
+          atividade_principal_id: cnaesSearch,
+          estado_id: process.env.ESTADO_ID,
+          situacao_cadastral: "Ativa"
+        },
+        token,
+        1
+      );
+
+      console.log("cnae id: " + cnaes[i].id + " - CNAE: " + cnaesSearch + " - total: " + data.paginacao.total + "  |" + (i + 1) + "º de " + cnaes.length + " | Páginas: " + data.paginacao.paginas);
+      console.log(" - - " + process.env.ACAO + " - - ");
+
+      var pageRound = process.env.PAGINAS;
+
+      if (data.paginacao.paginas < pageRound) {
+        pageRound = data.paginacao.paginas;
+      }
+
+      for (var t = 0; t < pageRound; t++) {
+        // --> Define uma página aleatória
+        if (data.paginacao.paginas > process.env.PAGINAS) {
+          page = Math.floor(Math.random() * data.paginacao.paginas) + 1;
+        } else {
+          page = t + 1;
+        }
+
+        console.log("Página: " + page + "  | " + (i + 1) + "º de " + cnaes.length);
+
+        // --> Pesquisa por página
+
+        const data2 = await consultarCNPJ.pesquisa(
+          {
+            // --> Consulta o CNAE
+            atividade_principal_id: cnaesSearch,
+            estado_id: process.env.ESTADO_ID,
+          },
+          token,
+          page
+        );
+
+        // --> Processamento da pesquisa no array industria
+        for (x = 0; x < data2.data.length; x++) {
+          const industria = await consultarCNPJ(data2.data[x], token); // --> Consulta o CNPJ
+
+          nome = industria.razao_social;
+          cnpj = industria.estabelecimento.cnpj;
+          fantasia = industria.estabelecimento.nome_fantasia;
+          endereco = industria.estabelecimento.tipo_logradouro + " " + industria.estabelecimento.logradouro;
+          numero = industria.estabelecimento.numero;
+          tipo_logradouro = industria.estabelecimento.tipo_logradouro;
+          complemento = industria.estabelecimento.complemento;
+          bairro = industria.estabelecimento.bairro;
+          cep = industria.estabelecimento.cep;
+          municipio = industria.estabelecimento.cidade.nome;
+          uf = industria.estabelecimento.estado.sigla;
+          pais = industria.estabelecimento.pais.nome;
+          ddd_telefone = industria.estabelecimento.ddd1;
+          telefone = industria.estabelecimento.telefone1;
+          email = industria.estabelecimento.email;
+          ddd_telefone2 = industria.estabelecimento.ddd2;
+          telefone2 = industria.estabelecimento.telefone2;
+          cnae = industria.estabelecimento.atividade_principal.id;
+          cadastro = industria.estabelecimento.situacao_cadastral;
+          if (industria.porte) {
+            porte = industria.porte.descricao;
+          } else {
+            porte = "";
+          }
+          produto_2 = industria.estabelecimento.atividade_principal.descricao;
+          produto_3 = industria.estabelecimento.atividades_secundarias;
+          tipo = industria.estabelecimento.tipo;
+          capital = industria.capital_social;
+          produto_1 = "Industria";
+          inscricao = "Regular";
+          filial = 0;
+          matriz = 0;
+          fundacao = industria.estabelecimento.data_inicio_atividade;
+
+          // filtros
+          if (fundacao != null) {
+            fundacao = fundacao.split("-");
+            fundacao = fundacao[0];
+          }
+
+          var atividade = [];
+          if (produto_3 !== null) {
+            for (n = 0; n < produto_3.length; n++) {
+              atividade.push(produto_3[n].descricao) + " ";
+            }
+          }
+
+          produto_3 = atividade.join();
+
+          if (tipo != null) {
+            if (tipo.includes("Matriz")) {
+              matriz = 1;
+            }
+
+            if (tipo.includes("Filial")) {
+              filial = 1;
+            }
+          }
+
+          const campos = ["nome", "cnpj", "fantasia", "endereco", "tipo_logradouro", "numero", "complemento", "bairro", "cep", "municipio", "uf", "pais", "ddd_telefone", "telefone", "email", "ddd_telefone2", "telefone2", "capital", "cnae", "produto_1", "produto_2", "produto_3", "matriz", "filial"];
+
+          for (const variavel of campos) {
+            if (!eval(variavel)) {
+              eval(`${variavel} = ""`);
+            }
+          }
+
+          var res = 0;
+
+          // --> Verifica se o CNPJ já está cadastrado no banco de dadso
+          const verificaDuplicata = await sequelize.query("SELECT cnpj FROM `catalogo` WHERE cnpj='" + cnpj + "'", {
+            type: QueryTypes.SELECT,
+          });
+
+          // --> Filtros para inserir no banco de dados
+          if (verificaDuplicata != "" || nome.includes("'") || endereco.includes("'") || bairro.includes("'") || municipio.includes("'") || telefone == "" || email == "" || fantasia.includes("'") || tipo_logradouro.includes("'")) {
+          } else {
+            try {
+              await sequelize.query("INSERT INTO catalogo (nome,cnpj, fantasia, endereco, tipo_logradouro, numero,complemento, bairro, cep, municipio, uf,pais,ddd_telefone, telefone, email, ddd_telefone2, telefone2, capital, cnae, produto_1, produto_2, produto_3, matriz, filial,ano_fundacao,produtos,materias_primas,nro_funcionarios,importa,exporta, porte) VALUES ('" + nome + "','" + cnpj + "','" + fantasia + "','" + endereco + "','" + tipo_logradouro + "','" + numero + "','" + complemento + "','" + bairro + "','" + cep + "','" + municipio + "','" + uf + "','" + pais + "','" + ddd_telefone + "','" + telefone + "','" + email + "','" + ddd_telefone2 + "','" + telefone2 + "','" + capital + "','" + cnae + "','" + produto_1 + "','" + produto_2 + "','" + produto_3 + "','" + matriz + "','" + filial + "','" + fundacao + "',0,0,0,0,0,'" + porte + "' ) ");
+              res = 1;
+            } catch {
+              console.log("ERRO MYSQL <-------------");
+            }
+          }
+          if (res == 0) {
+            res = "";
+          } else {
+            res = "    ✔ bd ";
+          }
+
+          console.log("  ✔  " + nome + res);
+        }
+
+        pages = await data.paginacao.paginas;
+      }
+      // --> Informa que o cnae já foi usado
+      await sequelize.query("UPDATE cnaes SET statusBot" + process.env.BOT + "=1 WHERE cnae=" + cnaesSearch + "");
+    }
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+getCNPJ();
